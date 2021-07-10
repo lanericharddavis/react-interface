e00a485e151c40209b89f5986e4410aa58edb9ac
Lane <lanericharddavis@gmail.com>
2021-07-10 16:51:47 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
+import { BiCalendar } from "react-icons/bi"
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo"
+import Search from "./components/Search"
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-800 align-top" />Your Appointments</h1>
+        <AddAppointment />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
+      
+      <ul className="divide-y divide-gray-200">
+        {filteredAppointments.map(appointment => (
+          <AppointmentInfo key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={
+              appointmentId =>
+                setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
+            }
+          />
+        ))
+        }
+      </ul>
     </div>
   );
 }
